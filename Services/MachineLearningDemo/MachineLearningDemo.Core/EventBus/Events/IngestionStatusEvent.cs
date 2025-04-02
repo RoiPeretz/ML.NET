55756d73bb96c8b60db523bc231b8cec1ec73f4e
Roi Peretz <Roip_Dev@outlook.com>
2025-04-02 18:44:08 +0300

    Bff + app - ingestion statuts integration. work in progress

--- Services/MachineLearningDemo/MachineLearningDemo.Core/EventBus/Events/IngestionStatusEvent.cs
+++ Services/MachineLearningDemo/MachineLearningDemo.Core/EventBus/Events/IngestionStatusEvent.cs
@@ -6,17 +6,23 @@ public abstract record IngestionStatusEvent : IntegrationEvent
 }
 
 public record IngestionStartedEvent : IngestionStatusEvent
-{ }
+{
+    public string Message { get; set; } = "Ingestion started";
+}
 
 public record AddedToObjectStorageEvent : IngestionStatusEvent
-{ }
+{
+    public string Message { get; set; } = "Added to object storage";
+}
 
 public record DetectionStartedEvent : IngestionStatusEvent
 {
     public string ModelName { get; set; } = string.Empty;
+    public string Message { get; set; } = "Detection started";
 }
 
 public record DetectionEndedEvent : IngestionStatusEvent
 {
     public string ModelName { get; set; } = string.Empty;
+    public string Message { get; set; } = "Detection completed";
 }
